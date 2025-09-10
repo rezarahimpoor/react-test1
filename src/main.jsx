7aa682964c3fe1f91f5856caa04bef02fc38567a
rezarahimpoor <reza137872@gmail.com>
2025-09-10 16:07:26 +0330

    test1-project-with-cllass-component

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,57 @@
-import { StrictMode } from 'react'
+import { Component, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+
+
+
+class Timer extends Component{
+  render(){
+    return(
+      <h2>
+        it is {new Date().toLocaleTimeString()}
+      </h2>
+    )
+  }
+}
+
+
+
+
+
+class Hello extends Component{
+  render(){
+    return(
+      <h1>سلام دوستان من</h1>
+    )
+  }
+}
+
+
+
+
+
+
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <Hello/>        
+        <Timer/>
+      </div>
+    ) 
+  }
+}
+
+
+
+
+
+
+
+
 const root = createRoot(document.getElementById('root'));
 const tick = ()=> {
-  const elem = (
-  <div>
-    <h1>سلام دوستان</h1>
-    <h3>ddd</h3>
-    <h2>
-      it is {new Date().toLocaleTimeString()}.</h2>
-  </div>
-)
-root.render(elem);
+root.render(<App/>);
 }
 setInterval(tick, 1000);
