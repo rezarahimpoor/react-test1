732de049d5f0a9e245425aa226a3f621b9a2ac17
rezarahimpoor <reza137872@gmail.com>
2025-09-10 12:30:59 +0330

    اولین کامیت پروژه

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = createRoot(document.getElementById('root'));
+const tick = ()=> {
+  const elem = (
+  <div>
+    <h1>سلام دوستان</h1>
+    <h2>
+      it is {new Date().toLocaleTimeString()}.</h2>
+  </div>
 )
+root.render(elem);
+}
+setInterval(tick, 1000);
