ee300eed678f9cda71fb903c9ca3684c4e53f7c8
rezarahimpoor <reza137872@gmail.com>
2025-09-17 17:50:11 +0330

    test1-project-css-import

--- src/main.jsx
+++ src/main.jsx
@@ -1,55 +1,34 @@
 import { Component, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
-
-
+import './App.css'
 
 class Timer extends Component{
   render(){
     return(
-      <h2>
+      <h2 className='timer'>
         it is {new Date().toLocaleTimeString()}
+        
       </h2>
     )
   }
 }
-
-
-
-
-
 class Hello extends Component{
   render(){
     return(
-      <h1>سلام دوستان من</h1>
+      <h1 >سلام دوستان من</h1>
     )
   }
 }
-
-
-
-
-
-
-
 class App extends Component {
   render(){
     return (
-      <div>
+      <div className='main'>
         <Hello/>        
         <Timer/>
       </div>
     ) 
   }
 }
-
-
-
-
-
-
-
-
 const root = createRoot(document.getElementById('root'));
 const tick = ()=> {
 root.render(<App/>);
